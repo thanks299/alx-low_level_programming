029ac8766a64c25b1789224adfb88d8e41e25d8c
thanks <thanksagbeble@gmail.com>
2023-09-07 15:03:28 -0700

    hello printf C programming language

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio>
+
+
+/**
+ *
+ * main - this is my main function to output a result when done
+ *
+ * return: 0 when is done
+ */
+int main(void)
+{
+	printf('with proper grammar, but the outcome is a piece of art,\n');
+	return (0);

afd0ab6c9288ee7cbebb1edd0925ef9d6632ac9a
thanks <thanksagbeble@gmail.com>
2023-09-07 15:30:05 -0700

    hello printf task 5 C programming language

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
-#include<stdio>
+#include<stdio.h>
 
 
 /**
  *
- * main - this is my main function to output a result when done
+ * main - this is my main function to output a result
  *
- * return: 0 when is done
+ * return: always 0 when is done
  */
 int main(void)
 {
-	printf('with proper grammar, but the outcome is a piece of art,\n');
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+}

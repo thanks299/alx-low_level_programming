b12e667e7d5da3e7cf496e6e26aa42eba356480e
thanks <thanksagbeble@gmail.com>
2023-09-07 14:14:27 -0700

    hello puts 0 task  of C language

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+/**
+ * main - this is my function main
+ *
+ *Return: make it 0
+*/
+
+int main(void)
+{
+	puts("\"Priogramming is like building a multilingual puzzle);
+	return (0);
+}

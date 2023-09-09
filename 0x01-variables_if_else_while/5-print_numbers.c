20e71d02859f7b83545c294fc2615b50a7e9dc9a
thanks <thanksagbeble@gmail.com>
2023-09-08 18:54:02 -0700

    display number on the screen in c programming language

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -To Prints numbers from 0 to 9.
+ *
+ * Return: make 0 (Successful)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}

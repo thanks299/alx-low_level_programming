afeaf7531f6709f40184de4afeb47601382b149c
thanks <thanksagbeble@gmail.com>
2023-09-08 18:14:05 -0700

    I play what is called the alphabetic game task 2 of c variable

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdioh.>
+
+/**
+ * main - To Prints the alphabet.
+ *
+ * Return: make 0 (Successful)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}

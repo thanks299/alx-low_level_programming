4bbad7821955a698db28993feba1d8e968e4bade
thanks <thanksagbeble@gmail.com>
2023-09-15 15:34:47 -0700

    C more functions more nested loop

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,29 +2,24 @@
 
 
 /**
-
  * print_numbers - print 0-9
-
+ *
  * return: void
-
+ *
  */
 
 
 void print_numbers(void)
-
 {
+	char c;
 
-        char c;
+for (c = '0'; c <= '9'; c++)
 
+	{
 
-        for (c = '0'; c <= '9'; c++)
+	_putchar(c);
 
-        {
-
-                _putchar(c);
-
-        }
-
-        _putchar('\n');
+	}
+	_putchar('\n');
 
 }

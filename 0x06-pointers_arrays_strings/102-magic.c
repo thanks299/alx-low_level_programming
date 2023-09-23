b4c6870cba819c8500a8d4fe5c666d6c2809f62e
thanks <thanksagbeble@gmail.com>
2023-09-23 09:59:43 -0700

    a dream doesnt become reality through magic task 10 of c more pointers arrays and strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,7 +7,6 @@ int a[5];
 int *p;
 a[2] = 1024;
 p = &n;
-
 /**
  * write your line of code here...
  * Remember:
@@ -16,7 +15,7 @@ p = &n;
  * - only one statement
  * - you are not allowed to code anything else than this line of code
  */
-
+;
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);

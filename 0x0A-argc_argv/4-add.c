17e939baeb11f9e0b026d97b0ef703c996cc4160
thanks <thanksagbeble@gmail.com>
2023-10-06 17:19:03 -0700

    C arguement task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,7 +12,9 @@
 
 int check_num(char *str)
 {
-/*Declaring variables*/unsigned int count;
+/*Declaring variables*/
+unsigned int count;
+
 count = 0;
 while (count < strlen(str)) /*count string*/
 {
@@ -35,15 +37,20 @@ return (1);
 
 int main(int argc, char *argv[])
 {
-/*Declaring variables*/int count;
+/*Declaring variables*/
+int count;
+
 int str_to_int;
+
 int sum = 0;
+
 count = 1;
 while (count < argc) /*Goes through the whole array*/
 {
 if (check_num(argv[count]))
 {
-str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/sum += str_to_int;
+str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+sum += str_to_int;
 }
 /*Condition if one of the number contains symbols that are not digits*/
 else

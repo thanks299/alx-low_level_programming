4dc244a6062a68bf46d7a86395b4470a10abf4c1
thanks <thanksagbeble@gmail.com>
2023-11-23 21:35:39 -0800

    bit c manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,7 +9,8 @@
 * Return: value of the bit, or -1 if there is an error
 */
 
-int get_bit(unsigned long int n, unsigned int index){
+int get_bit(unsigned long int n, unsigned int index)
+{
 unsigned long int divisor, check;
 if (index > (sizeof(unsigned long int) * 8 - 1))
 return (-1);

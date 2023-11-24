6d458e17f834b7141fbd2ad907b5531a00a42b89
thanks <thanksagbeble@gmail.com>
2023-11-23 21:33:40 -0800

    bit c manipulation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,7 +7,8 @@
 * @index: index
 * Return: 1 if success, -1 if error
 */
-int clear_bit(unsigned long int *n, unsigned int index){
+int clear_bit(unsigned long int *n, unsigned int index)
+{
 if (index > sizeof(n) * 8)
 return (-1);
 *n &= ~(1 << index);

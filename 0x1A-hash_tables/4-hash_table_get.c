0581e3f435c33c6ed368ed1452ef7343ca67f5e8
thanks299 <thanksagbeble@gmail.com>
2024-02-02 13:08:06 -0800

    harsh tables

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,66 +1,27 @@
 #include "hash_tables.h"
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
 /**
- * hash_table_set - Adds an element to the hash table
+ * hash_table_get - Retrieves a value associated with a key
  * @ht: Pointer to the hash table
  * @key: Key
- * @value: Value
  *
- * Return: 1 if it succeeded, 0 otherwise
+ * Return: Value associated with the element,
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *new_node;
-	char *value_duplicate;
+	hash_node_t *current_node;
 	unsigned long int index;
-	unsigned long int current_index;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return (0);
-
-	value_duplicate = strdup(value);
-	if (value_duplicate == NULL)
-		return (0);
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	current_index = index;
+	if (index >= ht->size)
+		return (NULL);
 
-	while (ht->array[current_index])
-	{
-		if (strcmp(ht->array[current_index]->key, key) == 0)
-		{
-			/* Key already exists, update the value and free the duplicate */
-			free(ht->array[current_index]->value);
-			ht->array[current_index]->value = value_duplicate;
-			return (1);
-		}
-		current_index++;
-		if (current_index == ht->size)
-			current_index = 0; /* Wrap around to the beginning if necessary */
-		if (current_index == index)
-			break; /* Entire hash table checked, break the loop */
-	}
+	current_node = ht->array[index];
+	while (current_node && strcmp(current_node->key, key) != 0)
+		current_node = current_node->next;
 
-	/* Allocate and initialize a new node */
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-	{
-		free(value_duplicate);
-		return (0);
-	}
-
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(value_duplicate);
-		free(new_node);
-		return (0);
-	}
-
-	new_node->value = value_duplicate;
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-
-	return (1);
+	return ((current_node == NULL) ? NULL : current_node->value);
 }

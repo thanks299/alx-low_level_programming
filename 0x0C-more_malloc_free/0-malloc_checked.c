29706f0b0b6fe1ee565ded602081d9317a912340
thanks299 <139892876+thanks299@users.noreply.github.com>
2024-05-18 20:05:55 +0100

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 
-void *malloc_checked(unsigned int b) {
-    void *p;
-    p = malloc(b);
-    if (p == NULL) {
-        exit(98);
-    }
-    return (p);
+void *malloc_checked(unsigned int b){
+	void *p;
+	p = malloc(b);
+	if (p == NULL){
+		exit(98);
+	}
+	return (p);
 }
